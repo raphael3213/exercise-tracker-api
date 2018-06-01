45aeb4aa89e21b4b301c6b41dd966d85b1faf4c7
Glitch (exer-tracker12214) <none>
2018-06-01 12:21:25 +0000

    🎠👚 Checkpoint ./adder.js:1174538/212 ./server.js:1174538/59 ./views/index.html:1174538/20

---
+++ adder.js
@@ -0,0 +1,11 @@
+var express=require('express');
+var router=express.Router();
+
+var app=express();
+
+router.post('/newuser',function(req,res,next)
+            {
+  
+  console.log("yo");
+}
+            );
